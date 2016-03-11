7b6ee60ccfd00cfcd697e83401ffd7e64257244f
Justin Edmund <justin@jedmund.com>
2016-03-11 04:40:53 -0800

    Project component
    Displays 2-column-wide projects. Linna helped!

---
+++ components/Project/Project.js
@@ -0,0 +1,57 @@
+import React, { Component, PropTypes } from 'react';
+import Button from '../Button';
+import './Project.styl';
+
+class Project extends Component {
+
+  static propTypes = {
+    about: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    i: PropTypes.number.isRequired
+  };
+
+  render() {
+    return this.renderProject()
+  }
+
+  renderProject() {
+    const { name, about, i } = this.props;
+    const classes = this.assignClasses(i)
+
+    return (
+      <li className={classes}>
+        <a href="#">
+          <div className="overlay"></div>
+          <div className="placeholder"></div>
+        </a>
+        <section>
+          <div className="info">
+            <a href="#" target="_blank">
+              <h3>{name}</h3>
+            </a>
+          </div>
+          <div className="action col-1-4">
+            <a href="#" target="_blank">
+              <Button className="grey" title="See project" />
+            </a>
+          </div>
+        </section>
+        <p>{about}</p>
+      </li>
+    );
+  }
+
+  assignClasses(i) {
+    var classes = []
+    classes.push("project col-1-2")
+
+    if (i % 2 == 0) {
+       classes.push("left")
+       classes.push("after")
+    }
+
+    return classes.join(" ")
+  }
+}
+
+export default Project;
