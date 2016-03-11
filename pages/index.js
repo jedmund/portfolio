1b721deb76465a2ece1e162a2ffca9a859d2b9ec
Justin Edmund <justin@jedmund.com>
2016-03-11 04:41:19 -0800

    Added Projects to Index page
    Still WIP

--- pages/index.js
+++ pages/index.js
@@ -5,18 +5,43 @@
  */
 
 import React, { Component } from 'react';
+import ProjectList from '../components/ProjectList';
+import Project from '../components/Project';
 import Section from '../components/Section';
 
-export default class extends Component {
+const projects = [
+  {
+    "name": "Maitsu",
+    "about": "Develop your hobbies and find new ones with Maitsu. Set your goals and make something new every week!"
+  }, {
+    "name": "Foundation",
+    "about": "Share your story with Foundation, a platform for creators to document, collaborate and publish their work to the world."
+  }, {
+    "name": "Pinterest",
+    "about": "Pinterest is a place to discover and save creative ideas that inspire you."
+  }
+];
 
+export default class extends Component {
   render() {
     return (
       <div>
-        <Section className="design" header="I make creative tools"><div /></Section>
-        <Section className="writing" header="I write about design and culture"><div /></Section>
-        <Section className="about" header="I work as a product designer"><div /></Section>
+        <Section className="design" header="I make creative tools">
+          <ProjectList>
+            { this.renderProjects() }
+          </ProjectList>
+        </Section>
+        <Section className="writing" header="I write about design and culture">
+        </Section>
+        <Section className="about" header="I work as a product designer">
+          <about />
+        </Section>
       </div>
     );
   }
 
+  renderProjects() {
+    return projects.map((project, i) => 
+      <Project i={i} name={project.name} about={project.about} />)
+  }
 }
