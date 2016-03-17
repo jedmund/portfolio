1289a0451f441066714d6e2f4645ce90b1ceaa99
Justin Edmund <justin@jedmund.com>
2016-03-16 23:04:57 -0700

    Section additions: `image` and `underlined`
    Section components can now display an optional hero image with the
    `image` prop. It takes an object with “1x” and “2x” keys.
    
    Section components now have a boolean `underlined` key that determines
    whether its title has an underline.

--- components/Section/Section.jsx
+++ components/Section/Section.jsx
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import Image from '../Image'
 import './Section.styl';
 
 class Section extends Component {
@@ -6,15 +7,26 @@ class Section extends Component {
   static propTypes = {
     header: PropTypes.string.isRequired,
     className: PropTypes.string.isRequired,
-    children: PropTypes.element.isRequired
+    image: PropTypes.object,
+    underlined: PropTypes.boolean
   };
 
   render() {
-    const { className, header, children } = this.props;
+    const { className, header, image, children, underlined } = this.props;
+
+    if (image) {
+      const normal = image["1x"]
+      const retina = image["2x"]
+
+      var imageElement = <Image src={normal} retina={retina} />
+      console.log(imageElement)
+    }
+
     return (
       <section className={className}>
         <header>
-          <h2>{header}</h2>
+          <h2 className={ underlined ? "underlined" : "" }>{header}</h2>
+          {imageElement}
         </header>
         {children}
       </section>
